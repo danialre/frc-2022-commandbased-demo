9051ec62d9826ea3e9db681243b2699a534d0b58
Blockinator1 <70295628+Blockinator1@users.noreply.github.com>
2022-01-27 17:10:44 -0700

    Sarcasm
    Add Sarcasm to Arson.java

--- src/main/java/frc/robot/commands/Arson.java
+++ src/main/java/frc/robot/commands/Arson.java
@@ -7,7 +7,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class Arson extends CommandBase {
-  /** Creates a new Arsosn. */
+  /** Creates a new Arsosn. */ // Great Grammer!
   public Arson() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
